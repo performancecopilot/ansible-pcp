6af694f599965efc81926d5621b9305b3210aad1
Rich Megginson <rmeggins@redhat.com>
2024-01-04 12:35:47 -0700

    ci: use latest version of ansible-lint 2.16; fix lint issues
    Use the latest supported ansible-lint github action which requires
    conversion to collection.  Add .ansible-lint to the converted
    collection.
    
    Add dependabot checking for latest versions of github actions
    
    Use `mode: "0NNN"` instead of `mode: 0NNN`.
    
    ansible-test requires supported version `>= 2.13.0` even though the code
    still supports earlier versions of ansible.
    
    Signed-off-by: Rich Megginson <rmeggins@redhat.com>

--- scripts/lsrcollection.sh
+++ scripts/lsrcollection.sh
@@ -3,4 +3,4 @@
 TOPDIR=$(pwd)
 LSRDIR="ansible_collections/$LSR_ROLE2COLL_NAMESPACE/$LSR_ROLE2COLL_NAME"
 mkdir -p "$TOX_WORK_DIR/$LSRDIR"
-cp -a "$TOPDIR"/* "$TOX_WORK_DIR/$LSRDIR"
+cp -a "$TOPDIR"/* "$TOPDIR/.ansible-lint" "$TOX_WORK_DIR/$LSRDIR"
